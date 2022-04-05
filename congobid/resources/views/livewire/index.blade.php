fe9b35e09191194b5748c7add3417f5f8ae03a59
Roger Mutoto <73588858+RogerMutoto@users.noreply.github.com>
2022-04-05 01:16:44 +0100

    Compte à rebours

--- congobid/resources/views/livewire/index.blade.php
+++ congobid/resources/views/livewire/index.blade.php
@@ -201,12 +201,18 @@
                                                     <h6>Date du début</h6>
                                                     <div id="header" class="header" >
                                                         <div class="countdown mt-2">
-                                                            <h5> {{ date('d-m-Y', strtotime($article->enchere->date_debut)) }} à {{ date('H:m', strtotime($article->enchere->heure_debut)) }} </h5>
+                                                            <h5>
+                                                                <span id="days"></span>
+                                                                <span id="hours"></span><span id="minutes"></span><span id="seconds"></span>
+                                                            </h5>
+                                                        </div>
+                                                            {{-- <h5> {{ date('d-m-Y', strtotime($article->enchere->date_debut)) }} à {{ date('H:m', strtotime($article->enchere->heure_debut)) }} </h5> --}}
                                                             {{-- <span id="clock" class="text-black"> --}}
                                                                 {{-- <h1 class="text-center" id="count-down-timer_{{ $article->id }}"></h1> --}}
                                                                 {{-- @if (now()->format('d-m-Y') > $article->enchere->date_debut ? 'match' : 'not match' }}
-                                                                @endif --}}
+                                                                @endif 
                                                             </span>
+                                                            --}}
                                                         </div>
                                                     </div>
                                                 </div>
@@ -374,3 +380,45 @@
                 </div>
             </div>
         </div>
+        <?php
+            $counterdownresult = "04 04, 2022 21:00:00";
+        ?>
+        
+      {{-- document.getElementById("days").innerHTML = ((days < 10 && days > 0) ? '0' + days : days) + "J" ;
+      document.getElementById("hours").innerHTML =((hours < 10 && hours > 0) ? '0' + hours : hours) + ":";
+      document.getElementById("minutes").innerHTML =((minutes < 10 && minutes > 0) ? '0' + minutes : minutes) + ":";
+      document.getElementById("seconds").innerHTML =((seconds < 10 && seconds > 0) ? '0' + seconds : seconds); --}}
+
+<script>
+    // ici tu insert la date de fin.
+    var countDownDate = new Date("04 07, 2022 12:00:00").getTime();
+    
+    
+    var x = setInterval(function() {
+    
+      
+      var now = new Date().getTime();
+        
+      
+      var distance = countDownDate - now;
+        
+     
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+      // voici le là oùles id sont appelés.
+      document.getElementById("days").innerHTML = ((days < 10 && days > 0) ? '0' + days : days) + "J" ;
+      document.getElementById("hours").innerHTML =  ((hours < 10 && hours > 0) ? '0' + hours : hours) + ":";
+      document.getElementById("minutes").innerHTML =  ((minutes < 10 && minutes > 0) ? '0' + minutes : minutes) + ":";
+      document.getElementById("seconds").innerHTML =  ((seconds < 10 && seconds > 0) ? '0' + seconds : seconds);
+    
+    
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+    </script>
+    
