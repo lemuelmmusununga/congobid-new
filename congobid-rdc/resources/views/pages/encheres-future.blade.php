8828e97fd2b9afac09fd338b78766089003a90fa
pedrien <pedrienk@gmail.com>
2022-04-06 14:26:13 +0200

    new push

--- congobid-rdc/resources/views/pages/encheres-future.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    @livewire('encheres.enchere-futures')
-
-@endsection
--- congobid-rdc/resources/views/pages/encheres-gagne.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    @livewire('encheres.enchere-fermes')
-
-@endsection
