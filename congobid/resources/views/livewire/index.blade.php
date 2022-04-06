1b2648c8d120746c1a0da7b91045e51dd823c4b1
Roger Mutoto <rogermutotomanu9@gmail.com>
2022-04-06 13:07:21 +0100

    update

--- congobid/resources/views/livewire/index.blade.php
+++ congobid/resources/views/livewire/index.blade.php
@@ -5,7 +5,7 @@
                 <div class="block-content-pageHome">
                     <div class="block-videos container">
                         <div class="row g-1">
-                            <div class="col-lg-4 col-5">
+                            <div class="col-lg-4 col-4">
                                 <div class="block-banner-sm">
                                     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                                         <div class="carousel-indicators">
@@ -82,7 +82,7 @@
                             <div class="col-lg-4 col-4">
                                 <div class="block-video-winner block-video">
                                     <div class="title">
-                                       Vidéos de Nos gagnants
+                                       Vidéos des gagnants
                                     </div>
                                     <a href="{{ route('clients.gagnants.index') }}">
                                         <div class="play-video">
@@ -93,7 +93,7 @@
                                     </a>
                                 </div>
                             </div>
-                            <div class="col-lg-4 col-3">
+                            <div class="col-lg-4 col-4">
                                 <div class="block-video-fonction block-video">
                                     <div class="title">
                                         Comment ça marche
@@ -268,10 +268,7 @@
                                                     <div id="header" class="header" >
                                                         <div class="countdown mt-2">
                                                             <h5> {{ date('d-m-Y', strtotime($article->enchere->date_debut)) }} à {{ date('H:m', strtotime($article->enchere->heure_debut)) }} </h5>
-                                                            {{-- <span id="clock" class="text-black"> --}}
-                                                                {{-- <h1 class="text-center" id="count-down-timer_{{ $article->id }}"></h1> --}}
-                                                                {{-- @if (now()->format('d-m-Y') > $article->enchere->date_debut ? 'match' : 'not match' }}
-                                                                @endif --}}
+
                                                             </span>
                                                         </div>
                                                     </div>
@@ -374,3 +371,8 @@
                 </div>
             </div>
         </div>
+
+      {{-- document.getElementById("days").innerHTML = ((days < 10 && days > 0) ? '0' + days : days) + "J" ;
+      document.getElementById("hours").innerHTML =((hours < 10 && hours > 0) ? '0' + hours : hours) + ":";
+      document.getElementById("minutes").innerHTML =((minutes < 10 && minutes > 0) ? '0' + minutes : minutes) + ":";
+      document.getElementById("seconds").innerHTML =((seconds < 10 && seconds > 0) ? '0' + seconds : seconds); --}}
