0f390a5b8d870aa68d94f303d2e766c3ed4fb9f4
pedrien <78202651+pedrien@users.noreply.github.com>
2023-04-12 17:47:20 +0200

    push

--- new-congobid-03-old/resources/views/auth/register.blade.php
+++ new-congobid-03-old/resources/views/auth/register.blade.php
@@ -31,14 +31,14 @@
             <div class="row justify-content-center">
                     <div class="col-lg-6">
                         <div class="block-content-card">
-                            <div class="card card-form" style="padding-top: 40px; padding-bottom: 40px; background: transparent;">
+                            <div class="card card-form">
                                 <!-- Validation Errors -->
                                 <x-auth-validation-errors class="mb-4" :errors="$errors" style="color:red;" />
 
                                 <form method="POST" action="{{ route('register') }}">
                                     @csrf
                                     <!-- Name -->
-                                    <div class="form-group row g-3 g-lg-3">
+                                    <div class="form-group row g-4 g-lg-3">
                                         @if ( Session::has('prenom') || Session::has('nom') )
                                             <div class="col-12">
                                                 <input value ="{{ Session::pull('prenom') }}" class="block mt-1 w-full form-control" type="text" name="prenom"  required />
@@ -47,11 +47,20 @@
                                                 <input value ="{{ Session::pull('nom') }}"  id="name" class="block mt-1 w-full form-control" type="text" name="name"  required />
                                             </div>
                                         @else
+
                                             <div class="col-12">
-                                                <input placeholder="Entrez votre prénom" class="block mt-1 w-full form-control" type="text" name="prenom"  required autofocus />
+                                                <div class="form-floating">
+                                                    <input placeholder="Entrez votre prénom" class="block mt-1 w-full form-control" type="text" name="prenom"  required autofocus />
+                                                    <label for="floatingInput" class="d-flex align-items-center">Votre prénom</label>
+                                                </div>
+
                                             </div>
                                             <div class="col-12">
-                                                <input placeholder="Entrez votre nom"  id="name" class="block mt-1 w-full form-control" type="text" name="name"  required autofocus />
+                                                <div class="form-floating">
+                                                    <input placeholder="Entrez votre nom"  id="name" class="block mt-1 w-full form-control" type="text" name="name"  required autofocus />
+                                                    <label for="floatingInput" class="d-flex align-items-center">Votre nom</label>
+                                                </div>
+
                                             </div>
                                         @endif
 
@@ -68,24 +77,41 @@
                                                     <input value="{{ Session::pull('ps') }}" id="pseudo" class="block mt-1 w-full rounded-pill form-control" type="text" name="pseudo"  required  /> <br>
                                                 </div>
                                             @else
+                                            <div class="form-floating">
                                                 <x-input placeholder="Entrez votre pseudonyme" id="pseudo" class="block mt-1 w-full form-control" type="text" name="pseudo"  required autofocus />
+                                                <label for="floatingInput" class="d-flex align-items-center">Votre pseudonyme</label>
+                                            </div>
+
                                             @endif
                                         </div>
                                             @if(Session::has('telephone'))
                                                 <div class="input-group  rounded-pill col-12 border-danger">
-                                                    <input id="phone" value="{{ Session::pull('tel') }}"  name="telephone" class="block mt-1  w-100" maxlength="14" type="tel" required style="width: 100%;outline:none;border:1px solid red;" />
+                                                    <input id="phone" value="{{ Session::pull('tel') }}"  name="telephone" class="form-control" maxlength="14" type="tel" required style="width: 100%;outline:none;border:1px solid red;" />
                                                 </div>
                                                 <span>
                                                     <p class="text-danger">{{ Session::pull('telephone') }} </p>
                                                 </span>
                                             @elseif (Session::has('tel'))
                                                 <div class="input-group  rounded-pill col-12 border-danger">
-                                                    <input id="phone" value="{{ Session::pull('tel') }}"  name="telephone" class="block mt-1  w-100" maxlength="14" type="tel" required />
+                                                    <input id="phone" value="{{ Session::pull('tel') }}"  name="telephone" class="form-control" maxlength="14" type="tel" required />
                                                 </div>
                                             @else
-                                                <div class="input-group  rounded-pill col-12">
-                                                    <input id="phone"  class="block mt-1  w-100" name="telephone" maxlength="14" type="tel" required style="width: 100%;outline:none" />
+                                            <div class="col-12">
+                                                <div class="input-group">
+                                                    <div class="select w-25">
+                                                        <select class="form-select form-control input-group-text text-center" aria-label="Default select example" style="border-radius: 50px 0px 0px 50px">
+                                                            <option selected>+243</option>
+                                                            <option value="1">One</option>
+                                                            <option value="2">Two</option>
+                                                            <option value="3">Three</option>
+                                                        </select>
+                                                    </div>
+                                                    <div class="form-floating w-75">
+                                                        <input id="phone"  class="form-control" name="telephone" placeholder="Numéro de téléphone" maxlength="14" type="tel" required style="width: 100%;border-radius: 0 50px 50px 0" />
+                                                        <label for="floatingInput" class="d-flex align-items-center">Numéro de téléphone</label>
+                                                    </div>
                                                 </div>
+                                            </div>
                                             @endif
                                             <!-- Email Address -->
                                             {{-- <div class="col-12">
@@ -94,36 +120,35 @@
                                             <!-- Password -->
                                             @if(Session::has('password'))
                                                 <div class="col-password col-12">
-                                                    <div class="show-password" id="show-password">
-                                                        <span class="iconify icon-hidden" data-icon="akar-icons:eye-slashed" id="icon_hidden"></span>
-                                                        <span class="iconify icon-show" data-icon="akar-icons:eye-open" id="icon_show"></span>
+                                                    <div class="form-floating">
+                                                        <input id="password" class="block mt-1 w-full border-danger" type="password" name="password" placeholder="Mot de passe" required autocomplete="new-password" class="form-control" style="border:1px solid red;" />
+                                                        <label for="floatingInput" class="d-flex align-items-center">Mot de passe</label>
                                                     </div>
-                                                    <input id="password" class="block mt-1 w-full border-danger" type="password" name="password" placeholder="Mot de passe" required autocomplete="new-password" class="form-control" style="border:1px solid red;" />
                                                 </div>
                                                 <div class="col-12 col-password">
-                                                    <div class="show-password show-password-1" id="show-password_two">
-                                                        <span class="iconify icon-hidden" data-icon="akar-icons:eye-slashed" id="icon_hidden_two"></span>
-                                                        <span class="iconify icon-show" data-icon="akar-icons:eye-open" id="icon_show_two"></span>
+                                                    <div class="form-floating">
+                                                        <input  type="password" name="password_confirmation" placeholder='Confirmer le mot de passe' id="password_confirmation" required autocomplete="new-password" class="form-control block mt-1 w-full border-danger" style="border:1px solid red;"/>
+                                                        <label for="floatingInput" class="d-flex align-items-center">Confirmer le mot de passe</label>
                                                     </div>
-                                                    <input  type="password" name="password_confirmation" placeholder='Confirmer le mot de passe' id="password_confirmation" required autocomplete="new-password" class="form-control block mt-1 w-full border-danger" style="border:1px solid red;"/>
+
                                                 </div>
                                                 <span>
                                                     <p class="text-danger">{{ Session::pull('password') }}</p>
                                                 </span>
                                             @else
                                                 <div class="col-password col-12">
-                                                    <div class="show-password" id="show-password">
-                                                        <span class="iconify icon-hidden" data-icon="akar-icons:eye-slashed" id="icon_hidden"></span>
-                                                        <span class="iconify icon-show" data-icon="akar-icons:eye-open" id="icon_show"></span>
+                                                    <div class="form-floating">
+                                                        <x-input id="password" class="block mt-1 w-full" type="password" name="password" placeholder="Mot de passe" required autocomplete="new-password" class="form-control" />
+                                                        <label for="floatingInput" class="d-flex align-items-center">Mot de passe</label>
                                                     </div>
-                                                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" placeholder="Mot de passe" required autocomplete="new-password" class="form-control" />
+
                                                 </div>
                                                 <div class="col-12 col-password">
-                                                    <div class="show-password show-password-1" id="show-password_two">
-                                                        <span class="iconify icon-hidden" data-icon="akar-icons:eye-slashed" id="icon_hidden_two"></span>
-                                                        <span class="iconify icon-show" data-icon="akar-icons:eye-open" id="icon_show_two"></span>
+                                                    <div class="form-floating">
+                                                        <x-input  type="password" name="password_confirmation" placeholder='Confirmer le mot de passe' id="password_confirmation" required autocomplete="new-password" class="form-control block mt-1 w-full" />
+                                                        <label for="floatingInput" class="d-flex align-items-center">Confirmer le mot de passe</label>
                                                     </div>
-                                                    <x-input  type="password" name="password_confirmation" placeholder='Confirmer le mot de passe' id="password_confirmation" required autocomplete="new-password" class="form-control block mt-1 w-full" />
+
                                                 </div>
                                             @endif
                                         <!-- Confirm Password -->
@@ -131,9 +156,9 @@
                                         <div class="col-12">
                                             <div class="form-check">
                                                 <input class="form-check-input outline-danger" type="checkbox" id="CheckCondition" required>
-                                                <label class="form-check-label" for="CheckCondition">
+                                                <label class="form-check-label" for="CheckCondition" style="font-size: 14px; font-weight:500">
                                                     J'ai lu et j'accepte les conditions générales et la politique de confidentialité de
-                                                    <a href="/politique-de-confidentialite">congobid.com</a>
+                                                    <a href="/politique-de-confidentialite" style="color: var(--orangeBtn)">congobid.com</a>
                                                 </label>
                                             </div>
                                         </div>
