c5fc85847940f47097f7151b6f7690f28848ef83
pedrien <78202651+pedrien@users.noreply.github.com>
2023-04-12 17:06:22 +0200

    push

--- new-congobid-03-old/resources/views/auth/login.blade.php
+++ new-congobid-03-old/resources/views/auth/login.blade.php
@@ -1,36 +1,33 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CongoBid | HomePage</title>
-    <link href="https://fonts.googleapis.com/css2?family=BioRhyme:wght@300;400&family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="{{ asset('css/intlTelInput.css') }}">
-</head>
-<body>
+@extends('layouts.app-page', ['contentNavbar' => false])
+@section('content')
+<div class="block-page">
     <div class="block-form">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-6 px-0">
-                    <div class="content-form">
-                        <a href="/" class="back">
-                            <span>
-                                <span class="iconify" data-icon="la:angle-left"></span>
-                            </span>
-                            Accueil
-                        </a>
-                        <div class="text-center">
-                            <img src="{{asset('images\logo\logo.png')}}" class="mb-4" alt="" srcset="">
-                            <h2>Connexion</h2>
-
+        <div class="banner-login">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="content-form">
+                            <a href="/" class="back">
+                                <span>
+                                    <span class="iconify" data-icon="la:angle-left"></span>
+                                </span>
+                                Accueil
+                            </a>
+                            <div class="text-center">
+                                <img src="{{asset('images/logo.png')}}" class="mb-4 logo-app" alt="logo du site" >
+                                <h2>Connexion</h2>
+                            </div>
+                            <div class="text-center w-100">
+                                <h6 class="text-center w-100" style="margin: 0">Vous n'avez pas de compte ?</h6>
+                                <a href="{{ route('register') }}" class="btn btn-3d-rounded-sm btn-article mt-3">Inscrivez-vous</a>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row justify-content-center">
                 <div class="col-lg-6">
                     <div class="block-content-card">
                         <div class="card card-form">
@@ -44,34 +41,41 @@
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
 
-                                <div class="form-group row g-3 g-lg-4">
+                                <div class="form-group row g-4 g-lg-4">
                                     <!-- Email Address -->
                                 <div class="col-12">
-
-
-                                    <input id="phone" name="telephone"  class="block mt-1  w-100" type="tel" required style="width: 100%; outline:none" />
-
+                                    <div class="form-floating">
+                                        <input id="phone floatingInput" name="telephone"  class="block mt-1 form-control w-100" placeholder="numéro téléphone"  type="tel" required style="width: 100%; outline:none" />
+                                        <label for="floatingInput" class="d-flex align-items-center">Numéro téléphone</label>
+                                    </div>
                                 </div>
 
-                            
+
                              <!-- Password -->
                              <div class="col-password col-12">
-                                <div class="show-password" id="show-password">
+                                {{-- <div class="show-password" id="show-password">
                                     <span class="iconify icon-hidden" data-icon="akar-icons:eye-slashed" id="icon_hidden"></span>
                                     <span class="iconify icon-show" data-icon="akar-icons:eye-open" id="icon_show"></span>
+                                </div> --}}
+                                <div class="form-floating">
+                                    <x-input id="password floatingInput" class="block mt-1 w-full" type="password" name="password" placeholder="Mot de passe" required autocomplete="new-password" class="form-control" />
+                                    <label for="floatingInput" class="d-flex align-items-center">Mot de passe</label>
                                 </div>
-                                <x-input id="password" class="block mt-1 w-full" type="password" name="password" placeholder="Mot de passe" required autocomplete="new-password" class="form-control" />
+
                             </div>
 
                             <!-- Remember Me -->
-                            <div class="col-12 d-flex justify-content-between">
+                            <div class="col-12 d-flex justify-content-center">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="newsletter" id="remember_me">
-                                    <label class="form-check-label" for="remember_me">
+                                    <label class="form-check-label" for="remember_me" style="font-size: 14px;font-weight:500;">
                                         {{ __('Se souvenir de moi') }}
                                     </label>
                                 </div>
-                                <div class="d-flex items-center justify-content-end">
+                            </div>
+
+                            <div class="col-12">
+                                <div class="d-flex aling-items-center justify-content-center">
                                     @if (Route::has('password.request'))
                                         <a class="resert-password" href="{{ route('password.request') }}">
                                             {{ __('Mot de passe oublié ?') }}
@@ -79,35 +83,13 @@
                                     @endif
                                 </div>
                             </div>
-
-
-                            <div class="col-12">
-                                <x-button class="ml-3 btn w-100">
+                            <div class="col-12 justify-content-center d-flex">
+                                <button class="btn btn-3d-rounded-sm w-75">
                                     {{ __('Connexion') }}
-                                </x-button>
+                               </button>
                             </div>
                                 </div>
                             </form>
-                            <div class="block-login-network">
-                                {{-- <div class="text-center mt-4">
-                                    <p class="separator">Ou</p>
-                                </div> --}}
-                                {{-- <div class="row justify-content-center">
-                                    <div class="col-5">
-                                        <a href="{{ route('login.google') }}" class="icon">
-                                            <span class="iconify" data-icon="flat-color-icons:google"></span>
-                                        </a>
-                                    </div>
-                                    <div class="col-5">
-                                        <a href="{{route('login.facebook')}}" class="icon">
-                                            <span class="iconify" data-icon="logos:facebook"></span>
-                                        </a>
-                                    </div>
-                                </div> --}}
-                                <div class="text-center mt-5" style="font-size: 1.3em">
-                                    <p class="link mb-0">Pas de compte ? <a href="/register">Inscrivez-vous</a></p>
-                                </div>
-                            </div>
                         </div>
 
                     </div>
@@ -115,28 +97,5 @@
             </div>
         </div>
     </div>
-
-
-    <script src="{{asset('js/intlTelInput.js')}}"></script>
-
-    <script>
-        var input = document.querySelector("#phone");
-        window.intlTelInput(input, {
-
-          utilsScript: "js/utils.js",
-        });
-    </script>
-<script>
-    var condition = document.getElementById('#CheckCondition');
-    var mail = document.getElementById('#CheckCondition');    </script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
-<script src="{{asset('js/main.js')}}"></script>
-</body>
-</html>
-
-
-
-
-
+</div>
+@endsection
--- new-congobid-03-old/resources/views/auth/register.blade.php
+++ new-congobid-03-old/resources/views/auth/register.blade.php
@@ -1,43 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CongoBid | HomePage</title>
-    <link href="https://fonts.googleapis.com/css2?family=BioRhyme:wght@300;400&family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="stylesheet" href="{{ asset('css/intlTelInput.css') }}">
 
-</head><body>
+@extends('layouts.app-page', ['contentNavbar' => false])
+@section('content')
+<div class="block-page">
     <div class="block-form">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-6 px-0">
-                    <div class="content-form">
-                        <a href="/" class="back">
-                            <span>
-                                <span class="iconify" data-icon="la:angle-left"></span>
-                            </span>
-                            Accueil
-                        </a>
-                        <div class="text-center">
-                            <img src="{{asset('images\logo\logo.png')}}" class="mb-4" alt="" srcset="">
-                            <h2>Inscription</h2>
-                        </div>
-                        <div class="text-center w-100">
-                            <div class="text-white center  mt-4" style="font-size: 1.3em">
-                                <h6 class="text-center w-100" style="margin: 0">Je suis déjà inscrit ! <a href="{{ route('login') }}" style="color: var(--primaryColor)">Connectez-vous</a></h6>
+        <div class="banner-login">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="content-form">
+                            <a href="/" class="back">
+                                <span>
+                                    <span class="iconify" data-icon="la:angle-left"></span>
+                                </span>
+                                Accueil
+                            </a>
+                            <div class="text-center">
+                                <img src="{{asset('images/logo.png')}}" class="mb-4 logo-app" alt="logo du site" >
+                                <h2>Inscription</h2>
+                            </div>
+                            <div class="text-center w-100">
+                                <h6 class="text-center w-100" style="margin: 0">Vous avez déjà un compte ?</h6>
+                                <a href="{{ route('login') }}" class="btn btn-3d-rounded-sm btn-article mt-3">Connectez-vous</a>
                             </div>
                         </div>
                     </div>
                 </div>
-
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row justify-content-center">
                     <div class="col-lg-6">
                         <div class="block-content-card">
-                            <div class="card card-form" style="padding-top: 40px; padding-bottom: 40px">
+                            <div class="card card-form" style="padding-top: 40px; padding-bottom: 40px; background: transparent;">
                                 <!-- Validation Errors -->
                                 <x-auth-validation-errors class="mb-4" :errors="$errors" style="color:red;" />
 
@@ -143,18 +137,21 @@
                                                 </label>
                                             </div>
                                         </div>
-                                        <div class="col-12">
+                                        {{-- <div class="col-12">
                                             <div class="form-check">
                                                 <input class="form-check-input" type="checkbox" name="newsletter" id="CheckMail">
                                                 <label class="form-check-label" for="CheckMail">
                                                     Je souhaite recevoir des notifications concernant les offres GRATUITES et autres
                                                 </label>
                                             </div>
-                                        </div>
-                                        <div class="col-12">
-                                            <x-button class="ml-4 btn w-100">
+                                        </div> --}}
+                                        <div class="col-12 justify-content-center d-flex">
+                                            <button class="btn btn-3d-rounded-sm w-75">
                                                 {{ __("S'inscrire") }}
-                                            </x-button>
+                                            </button>
+                                            {{-- <x-button class="btn ">
+
+                                            </x-button> --}}
                                         </div>
                                         </div>
 
@@ -169,21 +166,7 @@
             </div>
         </div>
     </div>
-    <script src="{{asset('js/intlTelInput.js')}}"></script>
+</div>
+@endsection
 
-    <script>
-        var input = document.querySelector("#phone");
-        window.intlTelInput(input, {
 
-          utilsScript: "js/utils.js",
-        });
-    </script>
-    <script>
-        var condition = document.getElementById('#CheckCondition');
-        var mail = document.getElementById('#CheckCondition');    </script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
-    <script src="{{asset('js/main.js')}}"></script>
-</body>
-</html>
